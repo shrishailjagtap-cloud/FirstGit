d539cec4cd537741c2dc390831ce143a73a48e8f
shrishail <shrishailjagtap@gmail.com>
2020-12-01 15:19:08 +0530

    my first ...

---
+++ src/com/Test.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Test {
+
+	public void show() {
+		
+		System.out.println("show---method");
+	}
+	
+	public static void main(String[] args) {
+		
+		Test t=new Test();
+		t.show();
+		
+	}
+}
